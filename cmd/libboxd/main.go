b5a6806a2cd002a2fa060a071b01040881667ed1
loafman1120 <kangjun1120@hotmail.com>
2026-08-21 09:01:43 +0800

    Fix bindings

--- cmd/libboxd/main.go
+++ cmd/libboxd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
@@ -15,17 +14,11 @@ import (
 	"google.golang.org/grpc"
 )
 
-type endpointInfo struct {
-	Network string `json:"network"`
-	Address string `json:"address"`
-}
-
 func main() {
 	basePath := flag.String("base-path", ".", "runtime base directory")
 	workingPath := flag.String("working-path", "", "sing-box working directory")
 	tempPath := flag.String("temp-path", "", "sing-box temporary directory")
 	locale := flag.String("locale", "", "locale identifier")
-	endpointFile := flag.String("endpoint-file", "", "write endpoint JSON to this file")
 	logMaxLines := flag.Int("log-max-lines", 300, "number of retained log lines")
 	debugMode := flag.Bool("debug", false, "enable debug mode")
 	flag.Parse()
@@ -46,22 +39,6 @@ func main() {
 		fatal(err)
 	}
 	defer server.Close()
-
-	info := endpointInfo{Network: server.Network(), Address: server.Endpoint()}
-	payload, err := json.Marshal(info)
-	if err != nil {
-		fatal(err)
-	}
-	if *endpointFile != "" {
-		if err := os.MkdirAll(filepath.Dir(*endpointFile), 0o700); err != nil {
-			fatal(err)
-		}
-		if err := os.WriteFile(*endpointFile, payload, 0o600); err != nil {
-			fatal(err)
-		}
-		defer os.Remove(*endpointFile)
-	}
-	fmt.Println(string(payload))
 
 	serveError := make(chan error, 1)
 	go func() { serveError <- server.Serve() }()
